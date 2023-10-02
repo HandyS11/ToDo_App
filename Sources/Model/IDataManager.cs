df1b4bcbc7a5e6b994445e0a41c736f7ace1a120
HandyS11 <valentin-clergue@orange.fr>
2023-10-02 19:13:40 +0200

    improve model

---
+++ Sources/Model/IDataManager.cs
@@ -0,0 +1,21 @@
+﻿namespace Model
+{
+    public interface IDataManager
+    {
+        // GET COUNT
+        Task<int> GetToDosCount();
+
+        // GETs
+        Task<ToDo> GetToDosById(Guid id);
+        Task<ToDo> GetToDosByTitle(string title);
+        Task<ToDo> GetToDosByCompletion(bool isDone);
+        Task<IEnumerable<ToDo>> GetSomeToDos(int size, int limit);
+        Task<IEnumerable<ToDo>> GetAllToDos();
+
+        // EDITs
+        Task<ToDo> AddTodo(ToDo todo);
+        Task<ToDo> UpdateTodo(Guid id, ToDo todo);
+        Task<bool> DeleteTodo(ToDo todo);
+
+    }
+}
