60bcbcab90df56afe301e77d49c2ccf0432f64ec
HandyS11 <valentin-clergue@orange.fr>
2023-10-02 19:13:28 +0200

    add vms

---
+++ Sources/VM/AddOrEditToDoVM.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Model;
+
+namespace VM
+{
+    public partial class AddOrEditToDoVM : ToDoVM
+    {
+        [ObservableProperty]
+        private bool isNewToDo = false;
+
+        [ObservableProperty]
+        private string editTitle;
+
+        [ObservableProperty]
+        private string editDescription;
+
+        public AddOrEditToDoVM() : base(new ToDo("EMPTY_TITLE")) { }
+
+        public void Clone(ToDoVM vm)
+        {
+            if (vm == null)
+            {
+                IsNewToDo = true;
+                Model = new ToDo("EMPTY_TITLE");
+            }
+            else
+            {
+                IsNewToDo = false;
+                Model = vm.Model;
+                EditTitle = vm.Title;
+                EditDescription = vm.Description;
+            }
+        }
+    }
+}
